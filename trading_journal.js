53b64d63b7399e4afd477234db9031e18cc8751d
Hridyesh03 <sharmahridyesh01@gmail.com>
2023-12-26 10:24:47 +0530

    updated footer section,wroked on client testimonials added pictures in it and made it's sliding automatic

--- trading_journal.js
+++ trading_journal.js
@@ -4,12 +4,13 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
+  autoplay:{
+    delay:2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  
 });
